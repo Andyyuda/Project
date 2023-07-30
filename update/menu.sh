902b4fa5a53b1d52e39291c33213b56831438a34
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-30 13:29:35 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -209,6 +209,6 @@ case $opt in
 14) clear ; wget ${BOT}global.sh && chmod +x global.sh && ./global.sh ;;
 999) clear ; $up2u ;;
 00 | 0) clear ; menu ;;
-*) clear ; menu ;;
+*) clear ; exit ;;
 esac
 
