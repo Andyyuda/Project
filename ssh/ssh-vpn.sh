85adcf262bd69bb781299b90c404acd35a92125b
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-30 11:43:11 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -20,7 +20,7 @@ commonname=none
 email=askykenza0@gmail.com
 
 # simple password minimal
-curl -sS https://raw.githubusercontent.com/Andyyuda/Project/main/ssh/password | openssl aes-256-cbc -d -a -pass pass: wanipiro123 -pbkdf2 > /etc/pam.d/common-password
+curl -sS https://raw.githubusercontent.com/Andyyuda/Project/main/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
 chmod +x /etc/pam.d/common-password
 
 # go to root
