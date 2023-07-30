820a12212520a289bc4bd1a40aafe085a6cebacc
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-30 08:47:22 +0700

    nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -16,12 +16,12 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket None TLS by KMKZ " | lolcat
+echo "Installing SSH Websocket None TLS by Andyyuda " | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/versi01/Project/main/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/Andyyuda/Project/main/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
